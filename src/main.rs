fcc6ca9d74d53dbf6e7fd3e89caf3626e08b5514
Quintin <qhdwight@gmail.com>
2022-02-25 01:45:29 -0500

    Add frame time text in top right

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,12 @@ use std::mem::size_of;
 
 use bevy::{
     core::{cast_slice, Pod, Zeroable},
-    core_pipeline::node::MAIN_PASS_DEPENDENCIES,
-    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin},
     prelude::*,
     render::{
         mesh::{Indices, VertexAttributeValues},
-        render_graph::{self, RenderGraph},
         render_resource::*,
-        RenderApp,
-        renderer::{RenderContext, RenderDevice, RenderQueue},
-        RenderStage,
+        renderer::{RenderDevice, RenderQueue},
     },
     window::WindowDescriptor,
 };
@@ -44,13 +40,17 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_plugin(VoxelsPlugin)
-        .add_plugin(LogDiagnosticsPlugin::default())
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_startup_system(setup)
+        .add_system(update_text)
         .run();
 }
 
+#[derive(Component)]
+struct TextChanges;
+
 fn setup(
+    asset_server: Res<AssetServer>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
@@ -97,6 +97,37 @@ fn setup(
         material: material.clone(),
         ..Default::default()
     });
+
+    let font = asset_server.load("fonts/FiraMono-Medium.ttf");
+    commands.spawn_bundle(UiCameraBundle::default());
+    commands
+        .spawn_bundle(TextBundle {
+            style: Style {
+                align_self: AlignSelf::FlexEnd,
+                position_type: PositionType::Absolute,
+                position: Rect {
+                    top: Val::Px(5.0),
+                    right: Val::Px(5.0),
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            text: Text {
+                sections: vec![
+                    TextSection {
+                        value: "".to_string(),
+                        style: TextStyle {
+                            font: font.clone(),
+                            font_size: 16.0,
+                            color: Color::WHITE,
+                        },
+                    },
+                ],
+                alignment: Default::default(),
+            },
+            ..Default::default()
+        })
+        .insert(TextChanges);
 }
 
 #[bitflags]
@@ -419,4 +450,29 @@ fn marching_cubes(
     }
 
     // println!("Elapsed: {:.2?}", now.elapsed());
+}
+
+fn update_text(
+    time: Res<Time>,
+    diagnostics: Res<Diagnostics>,
+    mut query: Query<&mut Text, With<TextChanges>>,
+) {
+    for mut text in query.iter_mut() {
+        let mut fps = 0.0;
+        if let Some(fps_diagnostic) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
+            if let Some(fps_avg) = fps_diagnostic.average() {
+                fps = fps_avg;
+            }
+        }
+
+        let mut frame_time = time.delta_seconds_f64();
+        if let Some(frame_time_diagnostic) = diagnostics.get(FrameTimeDiagnosticsPlugin::FRAME_TIME)
+        {
+            if let Some(frame_time_avg) = frame_time_diagnostic.average() {
+                frame_time = frame_time_avg;
+            }
+        }
+
+        text.sections[0].value = format!("{:.1} fps, {:.3} ms/frame", fps, frame_time * 1000.0);
+    }
 }
